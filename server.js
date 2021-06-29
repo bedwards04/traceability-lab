bdc9cee2706b3c25320356d82061dbea2047513a
Brendan Edwards <bdawg@Brendans-MacBook-Air.local>
2021-06-29 14:41:27 -0600

    added APM

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'fb636138509f454ab68cb4c59f3d35ff',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 
 app.use(express.json());
 
 app.get('/', function(req, res) {
+    rollbar.log('Hello world')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
